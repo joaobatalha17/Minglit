721ae95c7e202845b2c9b704e4ade087cfba5f5c
Joao Batalha <joaobatalha17@gmail.com>
2011-10-19 23:18:16 -0400

    Added index.html page, and some images

---
+++ test.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+<body>
+
+Agent: So who do you think you are, anyhow?
+<br />
+        
+<?php
+// define variables
+$name = 'Neo';
+$rank = 'Anomaly';
+$serialNumber = 1;
+
+// print output
+echo "Neo: I am <b>$name</b>, the <b>$rank</b>. You can call me by my serial number, <b>$serialNumber</b>.";
+?>
+
+</body>
+</html>

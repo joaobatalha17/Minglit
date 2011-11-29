974133410b0db4cf19b09bbcabe4e1384c41cc3e
Joao Batalha <joaobatalha17@gmail.com>
2011-11-29 01:05:41 -0500

    Added codeigniter framework

---
+++ application/views/login_form.php
@@ -0,0 +1,17 @@
+<?php $this->load->view('includes/header'); ?>
+
+<div id="login_form">
+
+	<h1>Minglit</h1>
+    <?php 
+	echo form_open('login/validate_credentials');
+	echo form_input('username', 'Username');
+	echo form_password('password', 'Password');
+	echo form_submit('submit', 'Login');
+	echo anchor('login/signup', 'Create Account');
+	echo form_close();
+	?>
+
+</div><!-- end login_form-->
+	
+<?php $this->load->view('includes/footer'); ?>

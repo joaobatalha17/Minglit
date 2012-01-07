6cbde610d74ec07fa13452c3be192d4fd590f806
David Thomas <davidthomas5412@gmail.com>
2012-01-07 01:35:00 -0800

    front end stuff, currently working on the game.php

---
+++ beta_front/game.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title> Minglit </title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+	<script language=JavaScript src="jQuery.js"></script>
+</head>
+
+<body>
+	<div id="game_wrapper">
+		<div id="main">
+			<div id="video_container">
+				<h2 style="border:2px solid black;width:870;margin:0 auto 0;background-color:lightgrey"> Who in this room Sky Dived last Summer? </h2>
+				<img class="video" src="img1.png">
+				<img class="video" src="img2.png">
+				<img class="video" src="img3.png">
+				<img class="video" src="img4.png">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
